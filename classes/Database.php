b31a077c494c0a376e792fccd36af51b1309b9c3
Fuad Mahmud Shad <fuademrite@gmail.com>
2025-05-19 06:34:51 +0300

    I made some changes to the databse and encryption file, although those are at the experimental stage
    as stated, I am just experimenting right njwo to see what works for me. I may need to chgange tgeh cpdes in the future

---
+++ classes/Database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $pdo;
+
+    public function __construct() {
+        $host = 'localhost';
+        $dbname = 'password_manager';
+        $username = 'root'; // need to chaneg this agter I have finisshed the project
+        $password = '';     // I will have the change this 
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+}
--- classes/Encryption.php
+++ classes/Encryption.php
@@ -1,18 +1,45 @@
 <?php
 class Encryption {
-    public function encrypt() {
+    private $cipher = 'aes-256-cbc';
+    
+    public function encrypt($data, $key) {
+        // Geenrating Vector for my prokeject
+        $ivLength = openssl_cipher_iv_length($this->cipher);
+        $iv = openssl_random_pseudo_bytes($ivLength);
         
+        // Encrypting the given the data
+        $encrypted = openssl_encrypt($data, $this->cipher, $key, 0, $iv);
+        if ($encrypted === false) {
+            throw new Exception('Encryption failed');
+        }
+        
+        // Combinimng IV and encrypted data 
+        return base64_encode($iv . $encrypted);
     }
 
-    public function decrypt() {
+    public function decrypt($data, $key) {
+        // Decoding the base64-encoded data
+        $data = base64_decode($data);
+        $ivLength = openssl_cipher_iv_length($this->cipher);
         
+        // Extractingg IV and encrypted data
+        $iv = substr($data, 0, $ivLength);
+        $encrypted = substr($data, $ivLength);
+        
+        // Decryption of the giving real the data
+        $decrypted = openssl_decrypt($encrypted, $this->cipher, $key, 0, $iv);
+        if ($decrypted === false) {
+            throw new Exception('Decryption failed');
+        }
+        
+        return $decrypted;
     }
 
     public function generateKey() {
-       
+        // Generate a random 32-byte key for AES-256
+        return bin2hex(openssl_random_pseudo_bytes(32));
     }
 }
 
 
-// I am continuing my process for creating skeleton codes
-// I will move on from this after I have a definite path I want to taake 
+// this is just a prototype code O am working with right now, |I will change these codes once i have creached a conclusiomn
